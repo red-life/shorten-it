9f31d778b926cce5524af2e0526ad928995f374d
Shayan <r3d.l1fe6@gmail.com>
2023-10-22 18:09:27 +0330

    url repository with cache decorator implemented

--- internal/repositories/url_with_cache.go
+++ internal/repositories/url_with_cache.go
@@ -2,9 +2,14 @@ package repositories
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/red-life/shorten-it/internal/models"
+	"github.com/red-life/shorten-it/internal/pkg/customerror"
 	"github.com/red-life/shorten-it/internal/ports"
 )
+
+const URLCachePrefix = "URL"
 
 func NewURLWithCacheRepository(cache ports.Cache, urlRepo ports.URLRepository) ports.URLRepository {
 	return &URLWithCache{
@@ -18,17 +23,44 @@ type URLWithCache struct {
 	urlRepo ports.URLRepository
 }
 
-func (U URLWithCache) Save(ctx context.Context, url models.URL) error {
-	//TODO implement me
-	panic("implement me")
+func (U *URLWithCache) Save(ctx context.Context, url models.URL) error {
+	err := U.urlRepo.Save(ctx, url)
+	if err != nil {
+		return err
+	}
+	longKey := fmt.Sprintf("%s:%s:long", URLCachePrefix, url.Key)
+	keyKey := fmt.Sprintf("%s:%s:key", URLCachePrefix, url.Long)
+	err = U.cache.Set(ctx, longKey, url.Long)
+	if err != nil {
+		return err
+	}
+	err = U.cache.Set(ctx, keyKey, url.Key)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (U URLWithCache) GetLongByKey(ctx context.Context, key string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (U *URLWithCache) GetLongByKey(ctx context.Context, key string) (string, error) {
+	keyKey := fmt.Sprintf("%s:%s:long", URLCachePrefix, key)
+	long, err := U.cache.Get(ctx, keyKey)
+	if err == nil {
+		return long, nil
+	}
+	if errors.Is(err, customerror.ErrNotFound) {
+		return U.urlRepo.GetLongByKey(ctx, key)
+	}
+	return "", err
 }
 
-func (U URLWithCache) GetKeyByLong(ctx context.Context, longURL string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (U *URLWithCache) GetKeyByLong(ctx context.Context, longURL string) (string, error) {
+	longKey := fmt.Sprintf("%s:%s:key", URLCachePrefix, longURL)
+	key, err := U.cache.Get(ctx, longKey)
+	if err == nil {
+		return key, nil
+	}
+	if errors.Is(err, customerror.ErrNotFound) {
+		return U.urlRepo.GetKeyByLong(ctx, longURL)
+	}
+	return "", err
 }
