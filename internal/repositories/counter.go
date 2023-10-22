d566d7787bfb3565964e08b4a363baa5da18b31a
Shayan <r3d.l1fe6@gmail.com>
2023-10-22 17:13:41 +0330

    counter repository implemented

--- internal/repositories/counter.go
+++ internal/repositories/counter.go
@@ -1,9 +1,15 @@
 package repositories
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/red-life/shorten-it/internal/ports"
 	"github.com/redis/go-redis/v9"
+	"strconv"
 )
+
+const CounterPrefix = "COUNTER"
 
 func NewCounterRepository(rdb *redis.Client) ports.CounterRepository {
 	return &Counter{
@@ -15,12 +21,21 @@ type Counter struct {
 	rdb *redis.Client
 }
 
-func (c Counter) GetCounter() (int, error) {
-	//TODO implement me
-	panic("implement me")
+func (c *Counter) GetCounter(ctx context.Context) (int, error) {
+	key := fmt.Sprintf("%s:counter", CounterPrefix)
+	val, err := c.rdb.Get(ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return 0, nil
+		}
+		return 0, err
+	}
+	counter, _ := strconv.Atoi(val)
+	return counter, nil
 }
 
-func (c Counter) Increase(i int) error {
-	//TODO implement me
-	panic("implement me")
+func (c *Counter) Increase(ctx context.Context) error {
+	key := fmt.Sprintf("%s:counter", CounterPrefix)
+	_, err := c.rdb.Incr(ctx, key).Result()
+	return err
 }
