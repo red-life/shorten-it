1121af47ebfcc0c735ee3d09f0c3fb1bcb0d6a79
Shayan <r3d.l1fe6@gmail.com>
2023-10-22 17:31:38 +0330

    cache implemented

--- internal/repositories/cache.go
+++ internal/repositories/cache.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"github.com/red-life/shorten-it/internal/ports"
 	"github.com/redis/go-redis/v9"
+	"time"
 )
+
+const DefaultTTL = 5 * time.Minute
 
 func NewCache(rdb *redis.Client) ports.Cache {
 	return &Cache{
@@ -16,17 +19,22 @@ type Cache struct {
 	rdb *redis.Client
 }
 
-func (c Cache) Set(ctx context.Context, key string, value string) error {
-	//TODO implement me
-	panic("implement me")
+func (c *Cache) Set(ctx context.Context, key string, value string) error {
+	return c.rdb.Set(ctx, key, value, DefaultTTL).Err()
 }
 
-func (c Cache) Get(ctx context.Context, key string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (c *Cache) Get(ctx context.Context, key string) (string, error) {
+	pipe := c.rdb.Pipeline()
+	pipe.Get(ctx, key)
+	pipe.ExpireXX(ctx, key, DefaultTTL)
+	cmds, err := pipe.Exec(ctx)
+	if err != nil {
+		return "", err
+	}
+	return cmds[0].(*redis.StringCmd).Val(), nil
 }
 
-func (c Cache) Delete(ctx context.Context, key string) error {
-	//TODO implement me
-	panic("implement me")
+func (c *Cache) Delete(ctx context.Context, key string) error {
+	c.rdb.Del(ctx, key) // based on redis docs, if the key doesn't exist it will be ignored, so it won't return any errors (https://redis.io/commands/del/)
+	return nil
 }
