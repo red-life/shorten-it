153125ca1822b886809b3661a38458f0815dce09
Shayan <r3d.l1fe6@gmail.com>
2023-10-23 21:35:48 +0330

    base62 dependency added

--- internal/services/keygen.go
+++ internal/services/keygen.go
@@ -1,13 +1,20 @@
 package services
 
-import "github.com/red-life/shorten-it/internal/ports"
+import (
+	"github.com/red-life/shorten-it/internal/ports"
+	"github.com/red-life/shorten-it/pkg/base62"
+)
 
-func NewKeyGenService(counter ports.CounterRepository) ports.KeyGenService {
-	return &KeyGen{}
+func NewKeyGenService(counter ports.CounterRepository, converter base62.Converter) ports.KeyGenService {
+	return &KeyGen{
+		counter:   counter,
+		converter: converter,
+	}
 }
 
 type KeyGen struct {
-	counter ports.CounterRepository
+	counter   ports.CounterRepository
+	converter base62.Converter
 }
 
 func (k KeyGen) GenerateKey() (string, error) {
