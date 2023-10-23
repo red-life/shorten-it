ddb6d7ff87b41d137139748cc648d9333fe33be9
Shayan <r3d.l1fe6@gmail.com>
2023-10-23 21:53:51 +0330

    key generator service implemented

--- internal/services/keygen.go
+++ internal/services/keygen.go
@@ -1,23 +1,34 @@
 package services
 
 import (
+	"context"
 	"github.com/red-life/shorten-it/internal/ports"
 	"github.com/red-life/shorten-it/pkg/base62"
+	"sync"
 )
 
 func NewKeyGenService(counter ports.CounterRepository, converter base62.Converter) ports.KeyGenService {
 	return &KeyGen{
 		counter:   counter,
 		converter: converter,
+		mutex:     sync.Mutex{},
 	}
 }
 
 type KeyGen struct {
 	counter   ports.CounterRepository
 	converter base62.Converter
+	mutex     sync.Mutex
 }
 
-func (k KeyGen) GenerateKey() (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (k *KeyGen) GenerateKey(ctx context.Context) (string, error) {
+	k.mutex.Lock()
+	defer k.mutex.Unlock()
+	counter, err := k.counter.Increase(ctx)
+	if err != nil {
+		return "", err
+	}
+	key := k.converter.Encode(counter)
+	return key, nil
+
 }
