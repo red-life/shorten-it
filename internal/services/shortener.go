431d27f37a046752b5042fed7452c8cc304bc003
Shayan <r3d.l1fe6@gmail.com>
2023-10-24 01:31:21 +0330

    shortener service implemented

--- internal/services/shortener.go
+++ internal/services/shortener.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"context"
+	"errors"
+	"github.com/red-life/shorten-it/internal/models"
+	"github.com/red-life/shorten-it/internal/pkg/customerror"
 	"github.com/red-life/shorten-it/internal/ports"
 )
 
@@ -17,12 +20,34 @@ type Shortener struct {
 	kgs     ports.KeyGenService
 }
 
-func (s Shortener) Shorten(ctx context.Context, url string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *Shortener) Shorten(ctx context.Context, url string) (string, error) {
+	key, err := s.urlRepo.GetKeyByLong(ctx, url)
+	if err != nil && errors.Is(err, customerror.ErrNotFound) {
+		return s.generateAndSave(ctx, url)
+	}
+	if err == nil && key != "" {
+		return key, nil
+	}
+	return "", err
 }
 
-func (s Shortener) GetLongURL(ctx context.Context, key string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *Shortener) GetLongURL(ctx context.Context, key string) (string, error) {
+	return s.urlRepo.GetLongByKey(ctx, key)
+}
+
+func (s *Shortener) generateAndSave(ctx context.Context, url string) (string, error) {
+	key, err := s.kgs.GenerateKey(ctx)
+	if err != nil {
+		return "", err
+	}
+	urlModel := models.URL{
+		Long: url,
+		Key:  key,
+	}
+	// TODO: implement the retry pattern
+	err = s.urlRepo.Save(ctx, urlModel)
+	if err != nil {
+		return "", err
+	}
+	return key, nil
 }
